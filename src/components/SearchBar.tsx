f20af75dd5dacce0f65ca06afd7b26b2f0df8c1e
James Vickers <jamesvickers@bb-mac-jv.home>
2021-03-16 16:22:41 +0000

    Add users search to HomeScreen

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,32 +1,23 @@
-import { useLazyQuery } from "@apollo/client";
-import React, { useCallback, useState } from "react";
-import { Button, Text, TextInput, View } from "react-native";
-import { SEARCH_USERS_QUERY } from "../gql/SearchUsersQuery";
+import { ApolloError } from "@apollo/client";
+import React from "react";
+import { Text, TextInput, View } from "react-native";
 
-export default function Searchbar(): JSX.Element {
-  const [searchTerm, setSearchTerm] = useState("");
-
-  const [
-    findUsers,
-    {
-      data,
-      // loading,
-      error,
-    },
-  ] = useLazyQuery(SEARCH_USERS_QUERY, {
-    variables: { searchTerm },
-    fetchPolicy: "no-cache",
-  });
-
-  console.log("search data: ", data);
-
-  const onSearch = useCallback(async () => {
-    try {
-      await findUsers();
-    } catch {
-      console.error(error);
-    }
-  }, [error, findUsers]);
+export default function Searchbar({
+  error,
+  searchTerm,
+  setSearchTerm,
+}: {
+  error?: ApolloError | undefined;
+  searchTerm: string;
+  setSearchTerm: (newText: string) => void;
+}): JSX.Element {
+  // const onSearch = useCallback(async () => {
+  //   try {
+  //     await findUsers();
+  //   } catch {
+  //     console.error(error);
+  //   }
+  // }, [error, findUsers]);
 
   return (
     <View style={{ backgroundColor: "white" }}>
@@ -36,7 +27,7 @@ export default function Searchbar(): JSX.Element {
         onChangeText={setSearchTerm}
         placeholder={"Search"}
       />
-      <Button title="Search" onPress={onSearch} />
+      {/* <Button title="Search" onPress={onSearch} /> */}
       {error && (
         <Text style={{ fontSize: 30, fontWeight: "bold" }}>
           Search failed, please try again :(
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,7 +1,7 @@
-import { useQuery } from "@apollo/client";
+import { useLazyQuery, useQuery } from "@apollo/client";
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, StatusBar, Text, View } from "react-native";
 import { FlatList } from "react-native-gesture-handler";
 import SafeAreaView from "react-native-safe-area-view";
@@ -13,6 +13,7 @@ import SignUp from "../components/SignUp";
 import { useUser } from "../components/User";
 import UserItem from "../components/UserItem";
 import { ALL_USERS_QUERY } from "../gql/AllUsersQuery";
+import { SEARCH_USERS_QUERY } from "../gql/SearchUsersQuery";
 import LikeSvg from "../images/like.svg";
 import { RootStackParams } from "../routes";
 
@@ -21,12 +22,31 @@ export default function HomeScreen(): JSX.Element {
     StackNavigationProp<RootStackParams, "Posts">
   >();
 
+  const [searchTerm, setSearchTerm] = useState("");
+
   const {
     data,
     // , error, loading
   } = useQuery(ALL_USERS_QUERY);
 
+  const [
+    findUsers,
+    {
+      data: findUsersData,
+      // loading,
+      error: findUsersError,
+    },
+  ] = useLazyQuery(SEARCH_USERS_QUERY, {
+    variables: { searchTerm },
+    fetchPolicy: "no-cache",
+  });
+
   const user = useUser();
+
+  useEffect(() => {
+    console.log("findingUsers...");
+    findUsers();
+  }, [searchTerm, findUsers]);
 
   return (
     <SafeAreaView
@@ -58,13 +78,30 @@ export default function HomeScreen(): JSX.Element {
             <StyledBackground>
               <StyledText>STYLISH</StyledText>
             </StyledBackground>
-            <FlatList
+            {/* <FlatList
               data={data.allUsers}
               keyExtractor={(item) => item.id}
               renderItem={({ item }) => <UserItem key={item.id} user={item} />}
               ListHeaderComponent={<Text>allUsers listHeader:</Text>}
+            /> */}
+            <SearchBar
+              error={findUsersError}
+              searchTerm={searchTerm}
+              setSearchTerm={setSearchTerm}
             />
-            <SearchBar />
+            {findUsersData && (
+              <FlatList
+                data={findUsersData.allUsers}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                  <UserItem key={item.id} user={item} />
+                )}
+                ListHeaderComponent={
+                  <Text>findUsers search results listHeader:</Text>
+                }
+                ListEmptyComponent={<Text>No user matched found</Text>}
+              />
+            )}
           </>
         ) : (
           <>
