0568a512e558e912c14ed23ac481bf28a838d428
James Vickers <jvickersdesign@gmail.com>
2021-06-21 13:24:11 +0100

    Move theme switch and signout button from ProfileTab to SettingsTab

--- src/screens/ProfileTab.tsx
+++ src/screens/ProfileTab.tsx
@@ -1,37 +1,17 @@
-import React, { useCallback } from "react";
-import { Button } from "react-native";
-import { StackNavigationProp } from "@react-navigation/stack";
-import {
-  CompositeNavigationProp,
-  useNavigation,
-} from "@react-navigation/native";
-import { ChatTabsParams, RootStackParams } from "../routes";
+import React from "react";
 import { SafeAreaViewDefault } from "../components/SafeAreaViewDefault";
 import { useUser } from "../components/User";
-import { ToggleThemeSwitch } from "../components/ToggleThemeSwitch";
 import { ProfileImage } from "../components/ProfileImage";
 import { Outer } from "../components/Outer";
 import { Text } from "../components/Text";
 
 export const ProfileTab = (): JSX.Element => {
-  const navigation = useNavigation<
-    CompositeNavigationProp<
-      StackNavigationProp<RootStackParams, "ChatStack">,
-      StackNavigationProp<ChatTabsParams, "Profile">
-    >
-  >();
   const user = useUser();
-
-  const goToSignOutScreen = useCallback(() => {
-    navigation.navigate("SignOut");
-  }, [navigation]);
 
   return (
     <SafeAreaViewDefault>
       <Outer>
         <Text>Profile</Text>
-        <Button title="Sign Out" onPress={goToSignOutScreen} />
-        <ToggleThemeSwitch />
         <ProfileImage source={user.profileImage?.image.publicUrlTransformed} />
       </Outer>
     </SafeAreaViewDefault>
--- src/screens/SettingsTab.tsx
+++ src/screens/SettingsTab.tsx
@@ -1,25 +1,34 @@
-// import {
-//   CompositeNavigationProp,
-//   useNavigation,
-// } from "@react-navigation/native";
-// import { StackNavigationProp } from "@react-navigation/stack";
-import React from "react";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import React, { useCallback } from "react";
 import { SafeAreaViewDefault } from "../components/SafeAreaViewDefault";
 import { Text } from "../components/Text";
 import { Outer } from "../components/Outer";
+import { ChatTabsParams, RootStackParams } from "../routes";
+import { ToggleThemeSwitch } from "../components/ToggleThemeSwitch";
+import { Button } from "../components/Button";
 
 export const SettingsTab = (): JSX.Element => {
-  // const navigation = useNavigation<
-  //   CompositeNavigationProp<
-  //     StackNavigationProp<ChatTabsParams, "Settings">,
-  //     StackNavigationProp<ChatStackParams>
-  //   >
-  // >();
+  const navigation = useNavigation<
+    CompositeNavigationProp<
+      StackNavigationProp<RootStackParams, "ChatStack">,
+      StackNavigationProp<ChatTabsParams, "Settings">
+    >
+  >();
+
+  const goToSignOutScreen = useCallback(() => {
+    navigation.navigate("SignOut");
+  }, [navigation]);
 
   return (
     <SafeAreaViewDefault>
       <Outer>
         <Text>Settings</Text>
+        <Button text="Sign Out" onPress={goToSignOutScreen} />
+        <ToggleThemeSwitch />
       </Outer>
     </SafeAreaViewDefault>
   );
